1b5a130197afe1ce47e3fb5096b9dbc9b39526ec
Roberto Barreda <roberto.barreda@gmail.com>
2013-11-03 23:29:23 +0100

    modified GSB model based on etsy work

--- safe-browsing/models.py
+++ safe-browsing/models.py
@@ -26,9 +26,42 @@ class BaseProvider(models.Model):
         abstract = True
 
 
-class GoogleSafeBrowsing(BaseProvider):
+class GSBAdd(BaseProvider):
     provider_key = 'google'
+    list_id = models.IntegerField()
+    add_chunk_num = models.IntegerField()
+    host_key = models.CharField(max_length=8, db_index=True)
+    prefix = models.CharField(max_length=64)
 
+    class Meta:
+        unique_together = ('list_id', 'add_chunk_num', 'host_key', 'prefix')
+
+class GSBSub(BaseProvider):
+    provider_key = 'google'
+    list_id = models.IntegerField()
+    add_chunk_num = models.IntegerField()
+    sub_chunk_num = models.IntegerField()
+    host_key = models.CharField(max_length=8)
+    prefix = models.CharField(max_length=64)
+
+    class Meta:
+        unique_together = ('list_id', 'add_chunk_num', 'host_key', 'prefix')
+        index_together = [('list_id', 'sub_chunk_num')]
+
+class GSBFullHash(BaseProvider):
+    list_id = models.IntegerField()
+    add_chunk_num = models.IntegerField()
+    fullhash = models.CharField(max_length=64)
+    create_ts = models.PositiveIntegerField()
+
+    class Meta:
+        unique_together = ('list_id', 'add_chunk_num', 'fullhash')
+
+class GSBrfd(BaseProvider):
+   next_attempt = models.PositiveIntegerField()
+   error_count = models.PositiveIntegerField()
+   last_attempt = models.PositiveIntegerField()
+   last_success = models.PositiveIntegerField()
 
 class Phishtank(BaseProvider):
     provider_key = 'phishtank'
