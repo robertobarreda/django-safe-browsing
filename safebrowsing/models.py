a9c3d17545f03ac6f2a1b2454134b69e06801c2a
Roberto Barreda <roberto.barreda@gmail.com>
2014-01-10 17:08:33 +0100

    fixed choices

--- safebrowsing/models.py
+++ safebrowsing/models.py
@@ -1,4 +1,5 @@
 import logging
+
 from django.db import models, IntegrityError
 
 logger = logging.getLogger('safebrowsing')
@@ -38,12 +39,12 @@ class GSB_BaseProvider(BaseProvider):
     GOOG_WHITEDOMAIN = 3
     GOOG_PHISH = 4
 
-    LIST_TYPE = (
+    LIST_TYPE = {
         GOOG_MALWARE: 'goog-malware-shavar',
         GOOG_REGTEST: 'goog-regtest-shavar',
         GOOG_WHITEDOMAIN: 'goog-whitedomain-shavar',
         GOOG_PHISH: 'googpub-phish-shavar',
-    )
+    }
 
     class Meta:
         abstract = True
@@ -52,7 +53,7 @@ class GSB_BaseProvider(BaseProvider):
 class GSB_Add(GSB_BaseProvider):
     provider_key = 'google'
     list_id = models.PositiveSmallIntegerField(
-        choices=GSB_BaseProvider.LIST_TYPE)
+        choices=GSB_BaseProvider.LIST_TYPE.iteritems())
     add_chunk_num = models.IntegerField()
     host_key = models.CharField(max_length=8, db_index=True)
     prefix = models.CharField(max_length=64)
@@ -64,20 +65,20 @@ class GSB_Add(GSB_BaseProvider):
 class GSB_Sub(GSB_BaseProvider):
     provider_key = 'google'
     list_id = models.PositiveSmallIntegerField(
-        choices=GSB_BaseProvider.LIST_TYPE)
+        choices=GSB_BaseProvider.LIST_TYPE.iteritems())
     add_chunk_num = models.IntegerField()
-    sub_chunk_num = models.IntegerField()
     host_key = models.CharField(max_length=8)
     prefix = models.CharField(max_length=64)
+    sub_chunk_num = models.IntegerField()
 
     class Meta:
         unique_together = ('list_id', 'add_chunk_num', 'host_key', 'prefix')
-        index_together = [('list_id', 'sub_chunk_num')]
+        # index_together = [('list_id', 'sub_chunk_num')]
 
 
 class GSB_FullHash(GSB_BaseProvider):
     list_id = models.PositiveSmallIntegerField(
-        choices=GSB_BaseProvider.LIST_TYPE)
+        choices=GSB_BaseProvider.LIST_TYPE.iteritems())
     add_chunk_num = models.IntegerField()
     fullhash = models.CharField(max_length=64)
     create_ts = models.PositiveIntegerField()
